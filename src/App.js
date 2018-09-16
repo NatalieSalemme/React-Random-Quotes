cd64c0294f5c90a988a946884cb47141c14946c4
Natalie Salemme <NatalieSalemme@gmail.com>
2018-09-15 17:28:55 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import { QuoteBox } from './QuoteBox';
+
+class App extends Component {
+  render() {
+    return (
+      <QuoteBox />
+    );
+  }
+}
+
+export default App;
